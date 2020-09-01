0a215b5e9aaac5580c21a4d905a4ba7c06a241fd
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-01 18:23:04 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,10 @@
 def turn_count(board)
-  
+  num_of_turns_played = 0
+  board.each do |space|
+    if "X" || "O"
+      num_of_turns_played += 1
+    end
+  num_of_turns_played
 end  
 
 def current_player(board)
